d004042cbe237ed4fa73ac97ac6e72f9cff67242
Ashish Rana <ashishjaat451@gmail.com>
2025-01-05 09:31:56 +0530

    remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 
 const App = () => {
-  // API_URL=GET https://newsapi.org/v2/everything?q=Apple&from=2025-01-01&sortBy=popularity&apiKey=API_KEY
-
   return (
     <>
       <Navbar />
       <div className="container py-5">
-      <Outlet />
+        <Outlet />
       </div>
       <Footer />
     </>
